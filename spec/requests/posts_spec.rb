2c0fa9e31ace512ef6da685d769e25d489499bae
enmanuelbayen <enmanuel0495@gmail.com>
2023-08-22 19:05:06 -0400

    posts controller test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
+  describe 'GET #index' do
+    before(:example) { get '/users/1/posts' }
+
+    it 'response status should be correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template should be rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body should include the correct placeholder' do
+      expect(response.body).to include('Posts')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
+  describe 'GET #show' do
+    before(:example) { get '/users/1/posts/1' }
+
+    it 'response status should be correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template should be rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body should include the correct placeholder' do
+      expect(response.body).to include('Post')
     end
   end
 end
