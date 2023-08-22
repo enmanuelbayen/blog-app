f097d14aacb0035c51ec03d6df192f447d965908
enmanuelbayen <enmanuel0495@gmail.com>
2023-08-22 19:04:45 -0400

    test for users controller and setup for rspec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
+  describe 'GET #index' do
+    before(:example) { get '/users' }
+
+    it 'response status should be correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template should be rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body should include the correct placeholder' do
+      expect(response.body).to include('Users')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
+  describe 'GET #show' do
+    before(:example) { get '/users/1' }
+
+    it 'response status should be correct' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'correct template should be rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body should include the correct placeholder' do
+      expect(response.body).to include('User')
     end
   end
 end
