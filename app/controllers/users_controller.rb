7c1ab1ab2ca429c6213031a7062c154546c88f3b
enmanuelbayen <enmanuel0495@gmail.com>
2023-08-23 23:15:16 -0400

    create render shared/partial for user/post and view for user show post profile
    refactor user/index for add partial render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
-  def index 
+  def index
     @users = User.all
     @posts = Post.where(author_id: params[:id])
     @comments = Comment.where(post_id: params[:id])
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(author_id: params[:id])
+  end
 end
