c7ed7bb2f72a9e1849689cbda45a3294e3a7040b
enmanuelbayen <enmanuel0495@gmail.com>
2023-08-24 22:08:08 -0400

    Method update in postController for handle form input

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,5 +14,20 @@ class PostsController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      flash.now[:error] = 'Failed to create the post.'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
