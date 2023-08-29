46e4b9625a36701a88e41e4778a831b03c6d21bf
enmanuelbayen <enmanuel0495@gmail.com>
2023-08-29 16:27:48 -0400

    setup for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Store uploaded files on the local file system (see config/storage.yml for options).
   
   config.active_storage.service = :local
@@ -43,6 +42,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
