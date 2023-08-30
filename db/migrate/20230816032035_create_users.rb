609fd4c531ac1d7e02bae81f7e731131385bdad1
enmanuelbayen <enmanuel0495@gmail.com>
2023-08-30 15:58:21 -0400

    Add gem CanCanCan, create model ability and add role column to user

--- db/migrate/20230816032035_create_users.rb
+++ db/migrate/20230816032035_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.text :photo
       t.text :bio
       t.integer :posts_counter, default: 0
+      t.string :role
 
       t.timestamps
     end
